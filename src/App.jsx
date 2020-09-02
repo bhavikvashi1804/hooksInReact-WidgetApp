3ecc64f77a6fda56e5421a69f1938d8e7ceb10b8
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-02 09:38:01 +0530

    map the items and apply a style but here is one problem we return div at map this adds extra css double border in web page

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 
 import Accordion from './Components/Accordion';
 
-const items=[
+const items = [
   {
-    title:'What is React?',
-    content:'React is JS front end library',
+    title: 'What is React?',
+    content: 'React is JS front end library',
   },
   {
-    title:'React is developed by whom?',
-    content:'Facebook',
+    title: 'React is developed by?',
+    content: 'Facebook',
   },
   {
-    title:'Can you able to create mobile application using React?',
-    content:'Yes you just to learn little bit more, React Native',
+    title: 'Can you able to create mobile application using React?',
+    content: 'Yes,you just to learn little bit more, React Native',
   },
 ];
 
-export default() =>{
+export default () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <br />
       <Accordion items={items} />
     </div>
   );
--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const Accordion=({items})=>{
+
+    const renderedItem=items.map(
+        item=>{
+            return (
+                <div key={item.title}>
+                    <div className="title active">
+                        <i className="dropdown icon"></i>
+                        {item.title}
+                    </div>
+                    <div className="content active">
+                        <p>{item.content}</p>
+                    </div>
+                </div>
+            );
+        }
+    );
+
     return (
-        <h1>
-            {items.length}
-        </h1>
+        <div className="ui styled accordion">
+             {renderedItem}
+        </div>
     );
 
 };
