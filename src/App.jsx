35439e4c57e75683dc5624023374211432bce5bb
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-02 09:17:34 +0530

    Create a accordion component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Accordion from './Components/Accordion';
+
+export default() =>{
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Accordion />
+    </div>
+  );
+}
+
+
