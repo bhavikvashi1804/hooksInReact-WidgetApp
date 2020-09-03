56bb65721aac278762d75b2f08d4aaf47be566d6
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-03 14:05:09 +0530

    build list

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,50 +2,54 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, updateTerm] = useState("");
-    const [results,setResults]=useState([]);
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
-    console.log(results);
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
 
-    useEffect(
-        () => {
-            const searchTerm=async ()=>{
-                const {data}=await axios.get(
-                    'https://en.wikipedia.org/w/api.php',
-                    {
-                        params:{
-                            action:'query',
-                            list:'search',
-                            origin:'*',
-                            format:'json',
-                            srsearch:term,
-                        }
-                    }
-                );
+            setResults(data.query.search);
+        };
 
-                setResults(data);
-            };
+        if(term){
+            search();
+        }
+       
+    }, [term]);
 
-            if(term){
-                searchTerm();
-            }
-            
-        },
-        [term]
-    );
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    {result.snippet}
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
             <div className="ui form">
                 <div className="field">
-                    <label>Enter Search Term:</label>
+                    <label>Enter Search Term</label>
                     <input
-                        className="input"
                         value={term}
-                        onChange={e => updateTerm(e.target.value)}
+                        onChange={(e) => setTerm(e.target.value)}
+                        className="input"
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };
