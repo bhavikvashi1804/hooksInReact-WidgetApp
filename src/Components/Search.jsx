cb2637b679655de232d898235862643fa22fba83
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-03 13:34:01 +0530

    Method 2: call API in useEffect

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,30 +1,27 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Search=()=>{
-    const [term,updateTerm]=useState("");
+const Search = () => {
+    const [term, updateTerm] = useState("");
 
     useEffect(
-        ()=>{
-            //in useEffect you cannot directly use await keyword to use that you habe to make separate function
-            const searchTerm=async() =>{
-               await axios.get("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=Programming");
-            }
-
-            searchTerm();
+        () => {
+            (async () => {
+                await axios.get("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=Programming");
+            })();
         },
         [term]
     );
 
-    return(
+    return (
         <div>
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term:</label>
-                    <input 
-                    className="input" 
-                    value={term}
-                    onChange={e=>updateTerm(e.target.value)}
+                    <input
+                        className="input"
+                        value={term}
+                        onChange={e => updateTerm(e.target.value)}
                     />
                 </div>
             </div>
