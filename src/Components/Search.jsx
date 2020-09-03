262c016aac3600a4b888a67a3df307369bcebc83
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-03 13:25:47 +0530

    roll back

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,13 +3,12 @@ import { useEffect } from 'react';
 
 const Search=()=>{
     const [term,updateTerm]=useState("");
-    const [term1,updateTerm1]=useState("");
 
     useEffect(
         ()=>{
             console.log('I at initial render & when my array value changes');
         },
-        [term,term1]
+        [term]
     );
 
     return(
