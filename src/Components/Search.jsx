4e951568fc2ba5a3f690ce33032b8198c282197b
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-03 13:25:06 +0530

    useEffect: run at if item change in array

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 
 const Search=()=>{
     const [term,updateTerm]=useState("");
+    const [term1,updateTerm1]=useState("");
 
     useEffect(
         ()=>{
-            console.log('I run after every render also for initial render');
+            console.log('I at initial render & when my array value changes');
         },
+        [term,term1]
     );
 
     return(
