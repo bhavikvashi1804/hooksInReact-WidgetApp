1b55f1945028919677582b3f237955badbfe8554
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-04 12:35:06 +0530

    Display only not selected option

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -4,6 +4,9 @@ const Dropdown=({options,selected,onSelectedChange})=>{
 
     const renderedOptions=options.map(
         (option)=>{
+            if(option.value===selected.value){
+                return null;
+            }
             return(
                 <div 
                     key={option.value} 
