cf87ba547bce4194efc603edd8c2a34d38686b56
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-11 19:16:53 +0530

    make a request to Google API

--- src/Components/Convert.jsx
+++ src/Components/Convert.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React,{ useEffect } from 'react';
+
+import axios from 'axios';
 
 
 //API_Key="AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
@@ -7,12 +8,23 @@ import { useEffect } from 'react';
 const Convert=({language,text})=>{
 
     useEffect(()=>{
-        console.log("New Language or text");
+        axios.post(
+            'https://translation.googleapis.com/language/translate/v2',
+            {},
+            {
+                params: {
+                q: text,
+                target: language.value,
+                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+                },
+            }
+        );
+      
     },[language,text]);
 
     return (
         <div>
-            
+
         </div>
     );
 
