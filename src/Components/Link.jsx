a988978ca6254add8338827108c928f3b6b2dc43
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-12 11:22:45 +0530

    when ctrl + onclick on link then open that tab in new tab

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 const Link=({className,href,children})=>{
     
     const onClick=(event)=>{
+        if(event.metaKey || event.ctrlKey){
+            return;
+
+        }
+        
+        
         //we made this because we donot want to full reload page and get all the chunks again
         event.preventDefault();
+        //display the correct url in addressbar 
         window.history.pushState({},'',href);
+        //navigate Event
+        const navEvent= new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
 
     };
 
